753d61f00ebe6c878eeb323129bc53e67e61260f
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-11-29 19:13:55 +0000

    RegisterTestExecutionFilter

--- src/Platform/Microsoft.Testing.Platform/Helpers/TestApplicationBuilderExtensions.cs
+++ src/Platform/Microsoft.Testing.Platform/Helpers/TestApplicationBuilderExtensions.cs
@@ -6,13 +6,18 @@ using System.Diagnostics.CodeAnalysis;
 using Microsoft.Testing.Platform.Builder;
 using Microsoft.Testing.Platform.CommandLine;
 using Microsoft.Testing.Platform.Extensions;
+using Microsoft.Testing.Platform.Requests;
+using Microsoft.Testing.Platform.Services;
 
 namespace Microsoft.Testing.Platform.Helpers;
 
+[Experimental("TPEXP", UrlFormat = "https://aka.ms/testingplatform/diagnostics#{0}")]
+[SuppressMessage("ApiDesign", "RS0016:Add public types and members to the declared API", Justification = "Experimental API")]
 public static class TestApplicationBuilderExtensions
 {
-    [Experimental("TPEXP", UrlFormat = "https://aka.ms/testingplatform/diagnostics#{0}")]
-    [SuppressMessage("ApiDesign", "RS0016:Add public types and members to the declared API", Justification = "Experimental API")]
     public static void AddTreeNodeFilterService(this ITestApplicationBuilder testApplicationBuilder, IExtension extension)
-        => testApplicationBuilder.CommandLine.AddProvider(() => new TreeNodeFilterCommandLineOptionsProvider(extension));
+    {
+        testApplicationBuilder.CommandLine.AddProvider(() => new TreeNodeFilterCommandLineOptionsProvider(extension));
+        testApplicationBuilder.TestHost.RegisterTestExecutionFilter(sp => new TreeNodeFilter(sp.GetCommandLineOptions()));
+    }
 }
